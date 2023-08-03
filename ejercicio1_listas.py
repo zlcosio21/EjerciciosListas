d2ddc75003b4aeec10ecb2ac5bf4096609146c72
Sebastián <126442441+zlcosio21@users.noreply.github.com>
2023-08-02 21:26:43 -0500

    Ejercicio 1

--- ejercicio1_listas.py
+++ ejercicio1_listas.py
@@ -1,32 +1,29 @@
-#Dada dos listas (lista1 y lista2), de como resultado dos listas resultantes, una de la suma entre lista1 y lista2, y otra de la resta.
-
-a = int(input("Ingrese la cantidad de elementos de la lista 1: "))
-b = int(input("Ingrese la cantidad de elementos de la lista 2: "))
+#Dada dos listas (lista1 y lista2), de como resultado la suma entre lista1 y lista2, y otra de la resta.
 
 lista1 = []
 lista2 = []
-suma = 0
-suma_lista1 = 0
-suma_lista2 = 0
+suma_listas = 0
+resta_listas = 0
 
-for i in range(1, a + 1):
-  elemento_lista1 = int(input(f"Ingrese el valor del elemento {i} para la lista 1: "))
-  if i != 0:
-    lista1.append(elemento_lista1)
-for e in range(1, b + 1):
-  elemento_lista2 = int(input(f"Ingrese el valor del elemento {e} para la lista 2: "))
-  if e != 0:
-    lista2.append(elemento_lista2)
-for i in lista1:
-  suma = suma + i
-  suma_lista1 = suma_lista1 + i
-for e in lista2:
-  suma = suma + e
-  suma_lista2 = suma_lista2 + e
+cant_lista1 = int(input("Ingrese la cantidad de elementos a agregar en la lista 1: "))
+cant_lista2 = int(input("Ingrese la cantidad de elementos a agregar en la lista 2: "))
 
-resta = suma_lista1 - suma_lista2
+if cant_lista1 >= 1 and cant_lista2 >= 1:
+    for i in range(1, cant_lista1 + 1):
+        elem_lista1 = int(input(f"Ingrese el valor {i} para la lista 1: "))    
+        lista1.append(elem_lista1)
+    for e in range(1, cant_lista2 + 1):
+        elem_lista2 = int(input(f"Ingrese el valor {e} para la lista 2: "))
+        lista2.append(elem_lista2)
+else:
+    print("Ambos numeros digitados deben positivos")
 
-print(f"Los valores de la lista 1 son {lista1}")
-print(f"Los valores de la lista 2 son {lista2}")
-print(f"La suma de las dos listas es {suma}")
-print(f"La resta de las dos listas es {resta}")
+for suma_resta in lista1:
+    suma_listas += suma_resta
+    resta_listas += suma_resta
+for suma_resta in lista2:
+    suma_listas += suma_resta
+    resta_listas -= suma_resta
+
+print(f"La suma entre las dos listas es {suma_listas}")
+print(f"La resta entre las dos listas es {resta_listas}")
