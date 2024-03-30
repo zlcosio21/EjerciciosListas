6b3ce228bc1e8efba5fe1852d2985b73ce6995ce
Sebastián <126442441+zlcosio21@users.noreply.github.com>
2024-03-30 13:18:28 -0500

    Update Exercise 2

--- ejercicio2_listas.py
+++ ejercicio2_listas.py
@@ -1,18 +1,5 @@
-#Dado un arreglo de números (numbers) y un número entero n, determine si n se encuentra en el arreglo. El programa debe imprimir True o False como corresponda.
+# Given an array of numbers (numbers) and an integer n, determine if n is in the array. 
+# The program should print True or False as appropriate.
 
-n = int(input("Ingrese un numero entero: "))
-a = int(input("Ingrese la cantidad de elementos del arreglo: "))
-
-arreglo = []
-
-for i in range(1, a + 1):
-  elementos_arreglo = int(input(f"Ingrese el valor del elemento {i} del arreglo: "))
-  arreglo.append(elementos_arreglo)
-
-for e in range(len(arreglo)):
-  if n == arreglo[e]:
-    print(True)
-    print(f"{n} se encuentra en el arreglo")
-    break
-  else:
-    print(False)
+def contains(numbers, n):
+    return n in numbers
